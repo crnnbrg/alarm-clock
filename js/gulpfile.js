90089a17d289e447d5d345ffefa2b5f5ce6b76e2
EdwardMudaida <eddieigo8@gmail.com>
2017-05-25 08:26:34 +0300

    Add build task

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -22,43 +22,56 @@ var lib = require('bower-files')({
 
 var browserSync = require('browser-sync');
 
-gulp.task('jshint', function() {
+gulp.task('jshint', function () {
 	return gulp.src(['js/*.js'])
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'));
 });
 
-gulp.task('concatInterface', function() {
+gulp.task('concatInterface', function () {
 	return gulp.src(['js/*-interface.js'])
 	.pipe(concat('allConcat.js'))
 	.pipe(gulp.dest('./tmp'));
 });
 
-gulp.task('jsBrowserify', ['concatInterface'], function() {
+gulp.task('jsBrowserify', ['concatInterface'], function () {
 	return browserify({ entries: ['./tmp/allConcat.js'] })
 	.bundle()
 	.pipe(source('app.js'))
 	.pipe(gulp.dest('./build/js'));
 });
 
-gulp.task('minifyScripts', ['jsBrowserify'], function() {
+gulp.task('minifyScripts', ['jsBrowserify'], function () {
 	return gulp.src('./build/js/app.js')
 	.pipe(uglify())
 	.pipe(gulp.dest('./buils/js'));
 });
 
-gulp.task('bowerJS', function() {
+gulp.task('jsBower', function () {
 	return gulp.src(lib.ext('js').files)
 	.pipe(concat('vendor.min.js'))
+	.pipe(uglify())
 	.pipe(gulp.dest('./build/js'));
 });
 
-gulp.task('bowerCSS', function() {
+gulp.task('cssBower', function () {
 	return gulp.src(lib.ext('css').files)
 	.pipe(concat('vendor.css'))
 	.pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('bower', ['bowerJS', 'bowerCSS']);
+gulp.task('bower', ['jsBower', 'cssBower']);
 
-gulp.task('jsB')
+gulp.task('clean', function () {
+	return del(['build', 'tmp']);
+});
+
+gulp.task('build', ['clean'], function () {
+	if (buildProduction) {
+		gulp.start('minifyScripts');
+	} else {
+		gulp.start('jsBrowserify');
+	}
+	gulp.start('bower');
+	gulp.start('cssBuild');
+});
